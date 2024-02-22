7775c530a6980b6c1f29aa72804829576a3fe719
unknown <poke301qup@gmail.com>
2024-02-23 04:51:59 +0900

    readmeの整備

--- CreateEnv.ps1
+++ CreateEnv.ps1
@@ -2,11 +2,12 @@ $currentDirectory = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 # /で区切られた最後の文字列を取得
 $currentDirectory = $currentDirectory -split "\\" | Select-Object -Last 1
 if ($currentDirectory -eq "VoiroStudioReleaseVer") {
-    Write-Host "成功"
+    Write-Host "正しい位置でこのファイルが実行されたので環境構築を開始します"
     python3 -m virtualenv localenv
     .\activate_localenv.ps1
     python.exe -m pip install --upgrade pip
     pip install -r requirements_20240220.txt
+    Write-Host "環境構築が完了しました。run.ps1を実行するとアプリが起動できます。"
 } else {
     Write-Host "このファイルの場所が移動しています。VoiroStudioReleaseVerフォルダでこのプログラムを実行してください。"
 }
