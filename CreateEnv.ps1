eef80376932f061ea3afacdb9c84addeb67ef8f4
mujurin1 <mujurin25@yahoo.co.jp>
2024-05-24 20:30:47 +0900

    CreateEnvの修正

--- CreateEnv.ps1
+++ CreateEnv.ps1
@@ -6,7 +6,7 @@ if ($currentDirectory -eq "VoiroStudio") {
     python3 -m virtualenv localenv
     .\activate_localenv.ps1
     python.exe -m pip install --upgrade pip
-    pip install -r requirements_20240301.txt
+    pip install -r requirements_20240524.txt
     Write-Host "環境構築が完了しました。sub_run.ps1を実行するとアプリが起動できます。"
 } else {
     Write-Host "このファイルの場所が移動しています。VoiroStudioフォルダでこのプログラムを実行してください。"
